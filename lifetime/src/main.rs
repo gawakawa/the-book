3e4f8bf0b5097f18cd300a38b0797fc90bcc06d0
gawakawa <mashurakiryu@icloud.com>
2025-06-07 19:28:56 +0900

    :sparkles: Lifetime

---
+++ lifetime/src/main.rs
@@ -0,0 +1,22 @@
+use std::fmt::Display;
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() { x } else { y }
+}
+
+fn main() {
+    let string1 = String::from("long string is long");
+    {
+        let string2 = String::from("xyz");
+        let result = longest_with_an_announcement(
+            string1.as_str(),
+            string2.as_str(),
+            "The longest string is determined!",
+        );
+        println!("The longest string is {}", result);
+    }
+}
