2ad48006361182358e5678781918d5d1d6fd2665
gawakawa <mashurakiryu@icloud.com>
2025-06-22 11:51:49 +0900

    :sparkles: Test

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,9 +2,33 @@ use rand::Rng;
 use std::cmp::Ordering;
 use std::io;
 
+pub struct Guess {
+    #[allow(dead_code)]
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        #[allow(clippy::manual_range_contains)]
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {}.",
+                value
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {}.",
+                value
+            );
+        }
+
+        Guess { value }
+    }
+}
+
 fn main() {
     println!("Guess the number");
-    
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
     loop {
@@ -31,5 +55,16 @@ fn main() {
                 break;
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "Guess value must be less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
