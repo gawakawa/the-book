a6e71f252d38fd74ed2bafcf229987a05cccd4c3
gawakawa <mashurakiryu@icloud.com>
2025-07-13 21:47:26 +0900

    :sparkles: Arc

--- concurrency/src/main.rs
+++ concurrency/src/main.rs
@@ -1,9 +1,48 @@
-use std::{sync::mpsc, thread, time::Duration, vec};
+use std::{
+    sync::{Arc, Mutex, mpsc},
+    thread,
+    time::Duration,
+    vec,
+};
 
 fn main() {
-    mpsc();
+    arc();
 }
 
+fn arc() {
+    let counter = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let counter = Arc::clone(&counter);
+        let handle = thread::spawn(move || {
+            let mut num = counter.lock().unwrap();
+
+            *num += 1;
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Result: {}", *counter.lock().unwrap());
+}
+
+#[allow(dead_code)]
+fn basic_mutex() {
+    let m = Mutex::new(5);
+
+    {
+        let mut num = m.lock().unwrap();
+        *num = 6;
+    }
+
+    println!("m = {:?}", m);
+}
+
+#[allow(dead_code)]
 fn mpsc() {
     let (tx, rx) = mpsc::channel();
 
