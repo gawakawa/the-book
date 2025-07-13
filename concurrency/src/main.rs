2784d662665cfabf4b2e5050a15870b1ddc4457a
gawakawa <mashurakiryu@icloud.com>
2025-07-13 20:49:56 +0900

    :sparkles: Concurrency

---
+++ concurrency/src/main.rs
@@ -0,0 +1,31 @@
+use std::{thread, time::Duration};
+
+fn main() {}
+
+#[allow(dead_code)]
+fn capture_with_move() {
+    let v = vec![1, 2, 3];
+
+    let handle = thread::spawn(move || {
+        println!("Here's a vector: {:?}", v);
+    });
+
+    handle.join().unwrap();
+}
+
+#[allow(dead_code)]
+fn handle_join() {
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..5 {
+        println!("hi number {} from the main thread!", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    handle.join().unwrap();
+}
